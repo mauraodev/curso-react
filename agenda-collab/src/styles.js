19f6b6951b4efa485acaf53c8cd5076817282d66
Mauro Nunes <maurao.dev@gmail.com>
2018-11-24 01:04:43 -0200

    Styles

--- agenda-collab/src/styles.js
+++ agenda-collab/src/styles.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
 
+const color = '#3b86ff';
+
 export const Card = styled.article`
-    background-color: #3b86ff;
+    font-family: 'Open Sans', sans-serif;
+    background-color: ${color};
     color: #fff;
+    text-align: center;
 `;
 
 export const Name = styled.h1`
@@ -15,4 +19,18 @@ export const Price = styled.h2``;
 
 export const Description = styled.p``;
 
-export const Button = styled.a``;
+export const Button = styled.a`
+    box-sizing: border-box;
+    line-height: 35px;
+    border-radius: 4px;
+    width: 120px;
+    height: 35px;
+    background-color: #fff;
+    color: ${color};
+
+    &:hover {
+        backgroud-color: ${color};
+        color: #fff;
+        border: 1px solid #fff;
+    }
+`;
